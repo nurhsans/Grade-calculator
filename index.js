72925964ae5a36b3832ab5e964a68baa7376215c
noobnur <nurhsans@gmail.com>
2019-05-18 12:30:43 +0800

    ECHO: split files

---
+++ index.js
@@ -0,0 +1,17 @@
+  // enter numbers
+  // click button
+  function calculateAverage() {
+    console.log(calculateAverage)
+    const stringNumbers = ""
+    const stringArray = stringNumbers.split(" ")
+    let num = 0
+    for (const item of stringArray) {
+      num += parseInt(item)
+    }
+  }
+  // calculate averageAndSet
+     // get the numbers
+     // add the numbers
+     // divide numbers by the amt of numbers
+    // set innerhtml with the answer
+
